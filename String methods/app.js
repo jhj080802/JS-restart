e477219b0ede6f7ca02e2b18296ee1b67a03b885
jhj080802 <s24015@gsm.hs.kr>
2024-07-03 19:31:18 +0900

    prettier

--- String methods/app.js
+++ String methods/app.js
@@ -28,18 +28,17 @@
 // hasCola("난 콜라 먹고싶다.");
 // hasCola("콜라"); // 0
 
-
 // 금칙어 : 콜라
 // includes
 // 문자가 있으면 true
 // 없으면 flase 반환
 
-function hasCola(str){
-    if(str.includes('콜라')){
-        console.log('금칙어가 있습니다.');
-    } else{
-        console.log('통과');
-    }
+function hasCola(str) {
+  if (str.includes("콜라")) {
+    console.log("금칙어가 있습니다.");
+  } else {
+    console.log("통과");
+  }
 }
 
 hasCola("사이다 먹고싶다."); // -1
